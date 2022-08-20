0a1c13b10daef3df32eed75c09e22541f35cc9c5
Markus Falb <markus.falb@mafalb.at>
2022-08-20 23:09:39 +0200

    class: do not print, just return

--- bin/containerimages.py
+++ bin/containerimages.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='containerimages')
 
     # Adding optional argument
-    parser.add_argument("-f", "--file", help="use file as input", default=sys.stdin)
+    parser.add_argument("-f", "--file", help="use file as input", default=None)
     parser.add_argument("action")
     parser.add_argument("--os", default=None)
     parser.add_argument("--flavor", default=None)
@@ -21,22 +21,26 @@ if __name__ == '__main__':
     parser.add_argument("--attribute")
 
     arguments = parser.parse_args()
-
     images = schema.Containerimages(arguments.file)
 
     if arguments.action == 'yaml2json':
-        images.yaml_to_json()
+        print(images.yaml_to_json(images))
     elif arguments.action == 'listos' and arguments.json:
-        images.list_os(format='json')
-    elif arguments.action == 'listos':
-        images.list_os()
+        print(images.list_os())
+    elif arguments.action == 'listos' and not arguments.json:
+        oses = images.list_os()
+        for os in oses:
+            print(os)
     elif arguments.action == 'listimages':
-        images.list_images(arguments.os)
+        imagelist = images.list_images(arguments.os)
+        for image in imagelist:
+            print(image)
     elif arguments.action == 'children':
-        images.get_children(arguments.os, arguments.flavor)
+        for child in images.get_children(arguments.os, arguments.flavor):
+            print(child)
     elif arguments.action == 'parentimage':
-        images.get_parentimage(arguments.os, arguments.flavor)
+        print(images.get_parentimage(arguments.os, arguments.flavor))
     elif arguments.action == 'parent':
-        images.get_parent(arguments.os, arguments.flavor)
+        print(images.get_parent(arguments.os, arguments.flavor))
     elif arguments.action == 'attribute':
-        images.get_attribute(arguments.os, arguments.flavor, arguments.attribute)
+        print(images.get_attribute(arguments.os, arguments.flavor, arguments.attribute))
--- plugins/module_utils/containerimages/schema.py
+++ plugins/module_utils/containerimages/schema.py
@@ -10,42 +10,36 @@ class Containerimages:
     def __init__(self, datafile=None):
         if datafile:
             datafile = open(datafile, 'r')
-        self.data = yaml.load(datafile, Loader=yaml.Loader)
+            self.data = yaml.load(datafile, Loader=yaml.Loader)
+        else:
+            self.data = yaml.load(sys.stdin, Loader=yaml.Loader)
         if datafile:
             datafile.close()
 
-    def yaml_to_json(self):
+    def yaml_to_json(self, images):
         """Return json."""
-        json.dump(self, sys.stdout)
+        return json.dumps(self.data)
 
     def list_os(self, format='text'):
-        """Print Operating Systems."""
-        if format == 'json':
-            json.dump(list(self.data["images"].keys()), sys.stdout)
-        else:
-            for os in self.data["images"].keys():
-                print(os)
+        """Return Operating Systems."""
+        return list(self.data["images"].keys())
 
     def list_images(self, os):
-        """Print a list of images for a given Operating System."""
+        """Return a list of images for a given Operating System."""
+        images = []
         for image in self.data["images"][os]:
-            print(image['flavor'])
+            images.append(image['flavor'])
+        return images
 
     def get_attribute(self, os, flavor, attribute):
-        """Print a attribute for a given image."""
+        """Return an attribute for a given image."""
         for __flavor in self.data['images'][os]:
             if (__flavor['flavor'] == flavor):
                 __data = __flavor
         if (attribute in __data.keys()):
-            print(__data[attribute])
+            return __data[attribute]
 
     def get_children(self, os, flavor):
-        """Print all images that the given image is a dependency for."""
-        children = self.__get_children(os, flavor)
-        for child in children:
-            print(child)
-
-    def __get_children(self, os, flavor):
         """Helper function for get_children()."""
         children = []
         for __flavor in self.data['images'][os]:
@@ -54,11 +48,11 @@ class Containerimages:
         if ('children' in __data.keys()):
             for child in __data['children']:
                 children.append(child)
-                children.extend(self.__get_children(os, child))
+                children.extend(self.get_children(os, child))
         return children
 
     def get_parentimage(self, os, flavor):
-        """Print the parentimage for a given image."""
+        """Return the parentimage for a given image."""
         __parentdata = {}
         for __flavor in self.data['images'][os]:
             if (__flavor['flavor'] == flavor):
@@ -69,19 +63,18 @@ class Containerimages:
                 if (__flavor['flavor'] == parent):
                     __parentdata = __flavor
             if 'base_image' in __parentdata.keys():
-                parentimage = __parentdata['base_image']
-                print(parentimage)
+                return __parentdata['base_image']
             else:
-                print("None")
+                return None
         else:
-            print("None")
+            return None
 
     def get_parent(self, os, flavor):
-        """Print the parent image of an image."""
+        """Return the parent image of an image."""
         for __flavor in self.data['images'][os]:
             if (__flavor['flavor'] == flavor):
                 __data = __flavor
         if 'parent' in __data.keys():
-            print(__data['parent'])
+            return __data['parent']
         else:
-            print('None')
+            return None
